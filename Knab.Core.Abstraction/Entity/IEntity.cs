41d62b6dcaab66c901122d8fdb8bfd613f97c488
hamzeh.sarlak <hamzeh.sarlak@channelengine.com>
2023-03-19 14:39:48 +0100

    added sln file and core abstraction

---
+++ Knab.Core.Abstraction/Entity/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Knab.Core.Abstraction.Entity
+{
+    /// <summary>
+    /// Entity must have a id
+    /// </summary>
+    public interface IEntity<out TKey> : IEntityId<TKey>
+    {
+    }
+
+    /// <summary>
+    /// Simple entity must have a guid 
+    /// </summary>
+    public interface IEntity : IEntity<Guid>
+    {
+    }
+}
---
+++ Knab.Core.Abstraction/Entity/IEntityId.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Knab.Core.Abstraction.Entity
+{
+    /// <summary>
+    /// Base entity is type can be overriden
+    /// </summary>
+    public interface IEntityId
+    {
+        object Id { get; }
+    }
+    /// <summary>
+    /// Custom entity type id
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IEntityId<out T> : IEntityId
+    {
+        new T Id { get; }
+    }
+    /// <summary>
+    /// Guid entity id
+    /// </summary>
+    public interface IEntityGuidId : IEntityId<Guid>
+    {
+    }
+    /// <summary>
+    /// String entity id
+    /// </summary>
+    public interface IEntityStringId : IEntityId<string>
+    {
+    }
+    /// <summary>
+    /// Int entity id
+    /// </summary>
+    public interface IEntityIntId : IEntityId<int>
+    {
+    }
+}
