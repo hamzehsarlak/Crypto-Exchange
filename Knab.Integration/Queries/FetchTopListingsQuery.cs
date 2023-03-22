380491b93f95d9b80ab5f3625caa87532d0719e0
hamzeh.sarlak <hamzeh.sarlak@channelengine.com>
2023-03-22 10:34:41 +0100

    added commands and queries

---
+++ Knab.Integration/Queries/FetchTopListingsQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Knab.Core.IMediatR;
+using Knab.Integration.Dtos.Listing;
+
+namespace Knab.Integration.Queries
+{
+    public class FetchTopListingsQuery : MediatRQueryBase<IEnumerable<ListingDto>>
+    {
+        public int Count { get; set; }
+    }
+}
